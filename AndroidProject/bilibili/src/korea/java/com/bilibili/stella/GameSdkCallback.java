154a8917b30d73ce48ea0b5caf2f55925b6a4eb8
liuxianfeng <liuxianfeng@bilibili.com>
2021-03-25 17:05:13 +0800

    korea

---
+++ AndroidProject/bilibili/src/korea/java/com/bilibili/stella/GameSdkCallback.java
@@ -0,0 +1,297 @@
+package com.bilibili.stella;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.gs.android.GSCallback;
+import com.gs.android.base.utils.LogUtils;
+import com.unity3d.player.UnityPlayer;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class GameSdkCallback extends com.unity3d.player.UnityPlayerNativeActivity implements GSCallback {
+
+    private static final String TAG = "Unity3DCallback";
+
+    public static final String CALLBACKTYPE_IsLogin = "IsLogin";
+    public static final String CALLBACKTYPE_Login = "Login";
+    public static final String CALLBACKTYPE_Logout = "Logout";
+    public static final String CALLBACKTYPE_Register = "Register";
+    public static final String CALLBACKTYPE_UserCenter = "UserCenter";
+    public static final String CALLBACKTYPE_GetUserInfo = "GetUserInfo";
+    public static final String CALLBACKTYPE_Pay = "Pay";
+    public static final String CALLBACKTYPE_Init = "Init";
+    public static final String CALLBACKTYPE_AccountInvalid = "AccountInvalid";
+    public static final String CALLBACKTYPE_GetFreeUrl = "GetFreeUrl";
+
+    public static final int StatusCode_Success = 10010;
+    public static final int StatusCode_Fail = 10012;
+
+    public static void unity3dSendMessage(String callbackType, int code, Object data) {
+        Log.d(TAG, "send message to Unity3D, callbackType =" + callbackType);
+        try {
+            JSONObject jobj = new JSONObject();
+            jobj.put("callbackType", callbackType);
+            jobj.put("code", code);
+            jobj.put("data", data);
+            UnityPlayer.UnitySendMessage("SdkManager", "OnReviceCallback", jobj.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    public static class BaseData {
+        public String merchant_id;
+        public String app_id;
+        public String server_id;
+        public String app_key;
+        public boolean debug;
+    }
+
+    public static class User {
+        public String userID;
+        public String userName;
+        public String access_token;
+        public int login_type;
+    }
+
+    public static class OrderInfo {
+        public long moneyAmount;
+        public String productName;
+        public String order_sign;
+        public String notify_url;
+        public int productCount;
+        public String tradeNo;
+        public String subject;
+        public String extInfo;
+    }
+
+    protected static BaseData sharedInstance = null;
+
+    protected OrderInfo orderInfo = null;
+    protected User userInfo = null;
+    protected boolean status = false;
+
+    public void SendLoginMessage(int code) {
+        LogUtils.d("###", "public SendLoginMessage");
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("username", userInfo.userName);
+            dat.put("nickname", userInfo.userName);
+            dat.put("uid", userInfo.userID);
+            dat.put("access_token", userInfo.access_token);
+            dat.put("login_type", userInfo.login_type);
+            unity3dSendMessage("Login", code, dat.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoginSuccess(Bundle arg0) {
+        UnityPlayerNativeActivity.User user = new UnityPlayerNativeActivity.User();
+        user.userID = arg0.getString("uid");
+        user.userName = arg0.getString("username");
+        user.access_token = arg0.getString("access_token");
+        user.login_type = arg0.getInt("login_type");
+
+        if (status) {
+            userInfo = user;
+            return;
+        }
+        if (userInfo == null) {
+            userInfo = user;
+            SendLoginMessage(StatusCode_Success);
+        } else {
+            if (user.userID.equalsIgnoreCase(userInfo.userID)) {
+                return;
+            } else {
+                userInfo = user;
+                SendLoginMessage(10011);
+            }
+        }
+    }
+
+    @Override
+    public void onLoginFailure(int code, String message) {
+        LogUtils.d("###", "登陆失败：" + code + " error code:" + message);
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("code", code);
+            dat.put("message", message);
+            unity3dSendMessage("Login", StatusCode_Fail, dat.toString());
+            userInfo = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLogout() {
+        LogUtils.d("###", "public void onLogout() :");
+        try {
+            LogUtils.d("###", "public void onLogout(),uid:" + this.userInfo.userID);
+            unity3dSendMessage("Logout", StatusCode_Success, "");
+            userInfo = null;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onPaySuccess(String out_trade_no, String order_no) {
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("out_trade_no", out_trade_no);
+            dat.put("bs_trade_no", order_no);
+            unity3dSendMessage("Pay", StatusCode_Success, dat.toString());
+            LogUtils.d("###", "Pay success ");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onPayFailure(int code, String message) {
+        LogUtils.d("###", "Pay fail");
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("out_trade_no", "");
+            dat.put("message", "[" + code + "]" + message);
+            unity3dSendMessage("Pay", StatusCode_Fail, dat.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onGetUserInfoSuccess(Bundle arg0) {
+        UnityPlayerNativeActivity.User userInfo = new UnityPlayerNativeActivity.User();
+        userInfo.userID = arg0.getString("uid");
+        userInfo.userName = arg0.getString("username");
+        userInfo.access_token = arg0.getString("access_token");
+        userInfo.login_type = arg0.getInt("login_type");
+
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("username", userInfo.userName);
+            dat.put("nickname", userInfo.userName);
+            dat.put("uid", userInfo.userID);
+            dat.put("access_token", userInfo.access_token);
+            dat.put("login_type", userInfo.login_type);
+            unity3dSendMessage("GetUserInfo", 10010, dat.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onGetUserInfoFailure(int code, String message) {
+        try {
+            userInfo = null;
+            JSONObject dat = new JSONObject();
+            dat.put("code", code);
+            dat.put("message", message);
+            unity3dSendMessage("GetUserInfo", 10011, dat.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onQueryHistoryOrdersSuccess(String s, String s1) {
+
+    }
+
+    @Override
+    public void onQueryHistoryOrdersFailure(int i, String s) {
+
+    }
+
+    @Override
+    public void onUserUpgrade(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onAccountInvalid() {
+
+    }
+
+    @Override
+    public void onShareSuccess(int i, String s) {
+
+    }
+
+    @Override
+    public void onShareFailure(int i, int i1, String s) {
+
+    }
+
+    @Override
+    public void onQueryExchangeProductsSuccess(List<String> list) {
+
+    }
+
+    @Override
+    public void onQueryExchangeProductsFailure(int i, String s) {
+
+    }
+
+    @Override
+    public void onReceiveExchangeProductSuccess(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    public void onReceiveExchangeProductFailure(int i, String s) {
+
+    }
+
+    @Override
+    public void onGameTermsSuccess(boolean enable_nighttime_push) {
+        LogUtils.d("###", "public void onGameTermsSuccess() :");
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("code", 0);
+            dat.put("enable_nighttime_push", enable_nighttime_push);
+            unity3dSendMessage("ShowGameTerms", StatusCode_Fail, dat.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onGameTermsRefuse() {
+        LogUtils.d("###", "public void onGameTermsRefuse() :");
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("code", 1);
+            dat.put("messagge", "onGameTermsRefuse");
+            unity3dSendMessage("ShowGameTerms", StatusCode_Fail, dat.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onGameTermsIgnore() {
+        LogUtils.d("###", "public void onGameTermsIgnore() :");
+        try {
+            JSONObject dat = new JSONObject();
+            dat.put("code", 2);
+            dat.put("message", "onGameTermsIgnore");
+            unity3dSendMessage("ShowGameTerms", StatusCode_Fail, dat.toString());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSwitchAccount() {
+
+    }
+}
---
+++ AndroidProject/bilibili/src/korea/java/com/bilibili/stella/UnityPlayerNativeActivity.java
@@ -0,0 +1,151 @@
+package com.bilibili.stella;
+
+import com.gs.android.GameSDK;
+import com.gs.android.base.utils.LogUtils;
+import com.unity3d.player.*;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class UnityPlayerNativeActivity extends GameSdkCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void init(final boolean debug, String merchant_id, String app_id, String server_id, String app_key) {
+        status = true;
+        sharedInstance = new BaseData();
+        sharedInstance.merchant_id = merchant_id;
+        sharedInstance.app_id = app_id;
+        sharedInstance.server_id = server_id;
+        sharedInstance.app_key = app_key;
+        sharedInstance.debug = debug;
+        GameSDK.getInstance().init(UnityPlayer.currentActivity, merchant_id, app_id, server_id, app_key, this);
+    }
+
+    public void notifyZone(String role_id, String role_name, int lv, String serverid, String server_name, String createTime) {
+        GameSDK.getInstance().notifyZone(server_name, role_id, role_name, serverid, server_name);
+    }
+
+    public void createRole(String role_id, String role_name, int lv, String serverid, String servername, String createTime) {
+        GameSDK.getInstance().createRole(role_id, role_name, serverid, servername);
+    }
+
+    public void levelUp(String id, String name, int lv, String serverid, String servername, String createTime, String levelUpTime) {
+
+    }
+
+    public void pay(int money, String prodectName, int productCount, String tradeNo, String subject, String extInfo, String notify_url, String order_sign) {
+        orderInfo = new OrderInfo();
+        orderInfo.moneyAmount = money;
+        orderInfo.productName = prodectName;
+        orderInfo.productCount = productCount;
+        orderInfo.tradeNo = tradeNo;
+        orderInfo.subject = subject;
+        orderInfo.extInfo = extInfo;
+        orderInfo.notify_url = notify_url;
+        orderInfo.order_sign = order_sign;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                GameSDK.getInstance().pay("cp_server_id", "cp_server_name", orderInfo.tradeNo, orderInfo.productName, orderInfo.notify_url, orderInfo.extInfo);
+            }
+        });
+    }
+
+    public void showSelectDialog() {
+        AlertDialog dialog = new AlertDialog.Builder(UnityPlayer.currentActivity)
+                .setMessage("确认退出吗？")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("退出", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        UnityPlayerNativeActivity.this.finish();
+                    }
+                })
+                .setCancelable(false).show();
+    }
+
+    public void login() {
+        status = false;
+        if (!checkIsLogin()) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    LogUtils.d("###", "UoLogin!!!!!");
+                    GameSDK.getInstance().login();
+                }
+            });
+        }
+    }
+
+    public void logout() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                GameSDK.getInstance().logout();
+            }
+        });
+    }
+
+    public void showGameTerms() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                GameSDK.getInstance().showGameTerms();
+            }
+        });
+    }
+
+    public String getUDID() {
+        return GameSDK.getInstance().getUDID();
+    }
+
+    // Quit Unity
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        GameSDK.getInstance().appDestroy(UnityPlayer.currentActivity);
+    }
+
+    // Pause Unity
+    @Override
+    protected void onPause() {
+        super.onPause();
+        GameSDK.getInstance().appOffline(UnityPlayer.currentActivity);
+    }
+
+    // Resume Unity
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GameSDK.getInstance().appOnline(UnityPlayer.currentActivity);
+    }
+
+    public boolean checkIsLogin() {
+        if (userInfo != null) {
+            SendLoginMessage(10010);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int getSdkType() {
+        return 4;
+    }
+
+    @Override
+    public int getLanguageType() {
+        return 3;
+    }
+}
