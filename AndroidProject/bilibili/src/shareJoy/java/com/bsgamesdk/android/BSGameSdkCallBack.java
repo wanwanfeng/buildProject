0ce85f5db878715f4353dbfa130c45c4c25407a5
liuxianfeng <liuxianfeng@bilibili.com>
2021-12-28 12:21:42 +0800

    cps同步bili

--- AndroidProject/bilibili/src/shareJoy/java/com/bsgamesdk/android/BSGameSdkCallBack.java
+++ AndroidProject/bilibili/src/shareJoy/java/com/bsgamesdk/android/BSGameSdkCallBack.java
@@ -11,34 +11,18 @@ import com.unity3d.player.UnityPlayerNativeActivity;
 
 public class BSGameSdkCallBack implements CallbackListener {
 
-    public static final String CALLBACKTYPE_IsLogin = "IsLogin";
-    public static final String CALLBACKTYPE_Login = "Login";
-    public static final String CALLBACKTYPE_Logout = "Logout";
-    public static final String CALLBACKTYPE_Register = "Register";
-    public static final String CALLBACKTYPE_UserCenter = "UserCenter";
-    public static final String CALLBACKTYPE_GetUserInfo = "GetUserInfo";
-    public static final String CALLBACKTYPE_Pay = "Pay";
-    public static final String CALLBACKTYPE_Init = "Init";
-    public static final String CALLBACKTYPE_AccountInvalid = "AccountInvalid";
-    public static final String CALLBACKTYPE_GetFreeUrl = "GetFreeUrl";
-
-
-    public static final int StatusCode_Success = UnityPlayerNativeActivity.StatusCode_Success;
-    public static final int StatusCode_Fail = UnityPlayerNativeActivity.StatusCode_Fail;
-
-    public static void callback(String callbackType, int code, Object data) {
-        UnityPlayerNativeActivity.unity3dSendMessage(callbackType, code, data);
-    }
-
     public String callbackType;
 
     public BSGameSdkCallBack(String callbackType) {
         this.callbackType = callbackType;
     }
 
+    public void unity3dSendMessage(Object data) {
+        UnityPlayerNativeActivity.unity3dSendMessage(callbackType, UnityPlayerNativeActivity.StatusCode_Success, data);
+    }
+
     @Override
     public void onSuccess(Bundle bundle) {
-
     }
 
     @Override
@@ -50,7 +34,7 @@ public class BSGameSdkCallBack implements CallbackListener {
 
             // 此处为操作失败时执行，返回值为BSGameSdkError类型变量，其中包含ErrorCode和ErrorMessage
             LogUtils.d("onFailed\nErrorCode : " + arg0.getErrorCode() + "\nErrorMessage : " + arg0.getErrorMessage());
-            callback(callbackType, StatusCode_Fail, dat.toString());
+            UnityPlayerNativeActivity.unity3dSendMessage(callbackType, UnityPlayerNativeActivity.StatusCode_Fail, dat.toString());
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -62,7 +46,7 @@ public class BSGameSdkCallBack implements CallbackListener {
             JSONObject dat = new JSONObject();
             dat.put("code", arg0.getErrorCode());
             dat.put("message", arg0.getErrorMessage());
-            callback(callbackType, StatusCode_Fail, dat.toString());
+            UnityPlayerNativeActivity.unity3dSendMessage(callbackType, UnityPlayerNativeActivity.StatusCode_Fail, dat.toString());
             // 此处为操作异常时执行，返回值为BSGameSdkError类型变量，其中包含ErrorCode和ErrorMessage
             LogUtils.d("onError\nErrorCode : " + arg0.getErrorCode() + "\nErrorMessage : " + arg0.getErrorMessage());
         } catch (Throwable e) {
